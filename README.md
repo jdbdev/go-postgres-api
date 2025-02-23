9a414ea40cd65f779f05bd35b91f74cc5a020551
jdbdev <juliendb.dev@gmail.com>
2025-02-23 10:11:13 -0400

    creating basic endpoints for Users and Projects

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Simple API build with Postgres prior to full project build. 
+
+Following and modifying tutorial at:
+https://www.youtube.com/watch?v=tIPCFW_nqZM
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
 )
 
 // STRUCTS
@@ -14,18 +15,26 @@ type routeResponse struct {
 }
 
 func main() {
-	fmt.Println("Simple API with Postgres")
+	log.Println("starting server...")
 
 	// http Router:
+	log.Println("setting up routes...")
 	router := http.NewServeMux()
 	router.HandleFunc("GET /", indexPage)
+	router.HandleFunc("POST /users/register", register)
+	router.HandleFunc("POST /users/login", login)
+	router.HandleFunc("POST /projects/create", createProject)
+	router.HandleFunc("POST /projects/update", updateProject)
 
 	// http server:
+	log.Println("listening on port 8000...")
 	err := http.ListenAndServe(":8000", router)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
+
+// MIDDLEWARE:
 
 // HANDLER FUNCTIONS:
 
@@ -34,4 +43,46 @@ func indexPage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	json.NewEncoder(w).Encode(routeResponse{Message: "Hello World"})
 	w.Write([]byte("index page"))
+
+	// Output request Header to console (testing only)
+	fmt.Println(r.Header)
+
+	// Better formatting using DumpRequest()
+	requestDump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("REQUEST: %s\n", string(requestDump))
+	_, err = w.Write(requestDump)
+	if err != nil {
+		return
+	}
 }
+
+// User - Register (POST):
+func register(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	json.NewEncoder(w).Encode(routeResponse{Message: "User Registration Page"})
+}
+
+// User - Login  (POST):
+func login(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	json.NewEncoder(w).Encode(routeResponse{Message: "Login Page"})
+}
+
+// Project - Create (POST):
+func createProject(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	json.NewEncoder(w).Encode(routeResponse{Message: "Create Project Page"})
+}
+
+// Project - Update (PUT):
+func updateProject(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	json.NewEncoder(w).Encode(routeResponse{Message: "Update Project Page"})
+}
+
+// Project - Get all projects:
+// Project - Get project instance:
+// Project - Delete project:
